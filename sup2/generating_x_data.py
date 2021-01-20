8a17c4ffdc8f86183bb531888dbb007fdef0953c
AdamStan <adam15stan@gmail.com>
2021-01-21 00:12:44 +0100

    preparing data for teaching and testing works

---
+++ sup2/generating_x_data.py
@@ -0,0 +1,58 @@
+import itertools
+import random
+
+
+
+
+
+def create_all_available_hours(min_hour, max_hour, days, how_long):
+    available_data = []
+    for d in days:
+        for h in range(min_hour, max_hour, how_long):
+            available_data.append([d, h, h + how_long])
+    return available_data
+
+
+def create_finite_amount_of_data(min_hour, max_hour, days, how_long, max_depth = 7):
+    all_data = []
+    available_data = create_all_available_hours(min_hour, max_hour, days, how_long)
+    max_iteration = len(available_data) - max_depth
+    print("Max iteration: " + str(max_iteration))
+    # big posibilities
+    for i in range(len(available_data), max_iteration, -1):
+        print("iteration: " + str(i))
+        data_from_iteration = list(itertools.combinations(available_data, i))
+        print(len(data_from_iteration))
+        all_data += data_from_iteration
+        data_from_iteration = None
+    # small posibilities
+    for i in range(2,max_depth):
+        print("iteration: " + str(i))
+        data_from_iteration = list(itertools.combinations(available_data, i))
+        print(len(data_from_iteration))
+        all_data += data_from_iteration
+        data_from_iteration = None
+    return all_data
+
+def create_for_2_hour(max_depth = 7):
+    av2 = create_finite_amount_of_data(8,19,[1,2,3,4,5],2, max_depth)
+
+    with open('dataX2.txt', 'a') as the_file:
+        for d in av2:
+            row = str(d)
+            row = row.replace("[", "").replace("]","").replace("(", "").replace(")", "")
+            the_file.write(row)
+            the_file.write("\n")
+
+def create_for_1_hour(max_depth = 5):
+    av1 = create_finite_amount_of_data(8,19,[1,2,3,4,5],1, max_depth)
+
+    with open('dataX1.txt', 'a') as the_file:
+        for d in av1:
+            row = str(d)
+            row = row.replace("[", "").replace("]","").replace("(", "").replace(")", "")
+            the_file.write(row)
+            the_file.write("\n")
+
+create_for_2_hour(6)
+create_for_1_hour(4)
--- sup2/prepare_data.py
+++ sup2/prepare_data.py
@@ -1,22 +1,21 @@
-import numpy as np
 import itertools
 
 # from testing_x_train_data import x_train2, y_train
 
 def prepare_x_train_data(x_train):
     new_data = []
-    for cos in x_train:
-        for index in range(len(cos), 55):
-            cos.append([0,0,0])
-        new_data.append(cos)
+    for av_hours in x_train:
+        for index in range(len(av_hours), 55):
+            av_hours.append([0,0,0])
+        new_data.append(av_hours)
     return new_data
 
 def prepare_y_train_data(y_train):
     new_data = []
-    for cos in y_train:
-        for index in range(len(cos), 55):
-            cos.append(0)
-        new_data.append(cos)
+    for propabilities in y_train:
+        for index in range(len(propabilities), 55):
+            propabilities.append(0)
+        new_data.append(propabilities)
     return new_data
 
 
@@ -33,9 +32,22 @@ def create_all_available_hours(min_hour, max_hour, days, how_long):
 def create_finite_amount_of_data(min_hour, max_hour, days, how_long):
     all_data = []
     available_data = create_all_available_hours(min_hour, max_hour, days, how_long)
-    for i in range(1, len(available_data)):
+    for i in range(1, len(available_data) + 1):
         print("iteration: " + str(i))
-        data_from_iteration = itertools.combinations(available_data, i)
-        print(data_from_iteration)
+        data_from_iteration = list(itertools.combinations(available_data, i))
+        # print(data_from_iteration)
         all_data += data_from_iteration
     return all_data
+
+l = create_finite_amount_of_data(8,19,[1,2,3,4,5],2)
+print(len(l))
+# for d in l:
+#     print("zestaw")
+#     print(d)
+
+with open('dataX2.txt', 'a') as the_file:
+    for d in l:
+        # print("zestaw")
+        # print(d)
+        the_file.write(str(d))
+        the_file.write("\n")
